949261b20e4a9f9509bb181f347b1131aa961e60
Gustavo <gustavoleck@hotmail.com>
2021-12-21 00:31:14 -0300

    Corrigido conflito de commit anterior no arquivo Brook.js #005

--- Brook.js
+++ Brook.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
- Discord = require("discord.js"); 
+const Discord = require("discord.js"); 
 const client = new Discord.Client({intents: 32767});
 const config = require("./config.json"); 
-=======
-const Discord = require("discord.js");
-const client = new Discord.Client({
-    intents: [
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING
-    ]
-});
-
-const Config = require("./Config.json");
->>>>>>> 23c45583f195dc987a60653036abdd2e9376d6d8
 
 client.login(config.token); 
 
@@ -20,14 +8,7 @@ client.once('ready', async () => {
 
     console.log("✅ - Estou online!")
 
-<<<<<<< HEAD
 })
-=======
-client.once('ready', async () =>{
-    console.log('O bot foi iniciado.');
-    client.user.setActivity("", {type:'LISTENING'});
-});
->>>>>>> 23c45583f195dc987a60653036abdd2e9376d6d8
 
 client.on('messageCreate', message => {
      if (message.author.bot) return;
