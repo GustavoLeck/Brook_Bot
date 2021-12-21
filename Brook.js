8ebe21824258eef237115d2dae3692204f103d0e
Gustavo <gustavoleck@hotmail.com>
2021-12-20 23:11:05 -0300

    Alterado para V13 e preparando para POO #002

--- Brook.js
+++ Brook.js
@@ -1,36 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const yt = require('ytdl-core');
 const Config = require('./Config.json');
-const ConfigServer = require('./ConfigServer.json');
-const servidores = ConfigServer.server.connection;
-
-
-client.on ("ready", () => {
-    console.log('O bot foi iniciado.');
-});
-
-client.on("message", async (msg) => {
-    
-    //Filtros
-    if (!msg.guild) return;
-    
-    if (!msg.content.startsWith(Config.prefix))return;
-    
-    if (!msg.member.voice.channel){
-    msg.reply('Você não está em nenhum canal de voz seu cocô!!!');
-    return;
-    }
-    //Comandos
-    if (msg.content === Config.prefix + "ping"){
-        servidores = await Discord.Message.channel.send("Calculando...")
-        servidores.edit('Ping de ${m.createdTimestamp - message.createdTimestamp}ms. A latencia da API é ${Math.round(client.ping)}ms')
-    }else;
-
-    if (msg.content === Config.prefix + "parar"){
-        servidores = await msg.member.voice.channel.leave();
-        msg.channel.send("fora");
-    }else;
-});
 
 client.login(Config.token);
+
+client.once('ready', async () =>{
+    console.log('O bot foi iniciado.');
+})
+
+client.on('messageCreate', () => {
+    if (message.author.bot) return;
+    if (message.channel.type == 'dm')return;
+    if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase()))return;
+    if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`))return;
+
+    const args = message.content
+    .trim().slice(config.prefix.length)
+    .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try{
+    const commandFile = require(`.commands/${command}.js`)
+    } catch (err){
+        console.error('Erro' + err);
+    }
+});
